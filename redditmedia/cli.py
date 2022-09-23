cd4810dd483d3ab92b1c08b4bb66c6543ae6738a
Caps Lock <46106832+capsey@users.noreply.github.com>
2022-09-23 18:28:43 +0600

    Formatting and typing fixed

--- redditmedia/cli.py
+++ redditmedia/cli.py
@@ -1,8 +1,9 @@
 import click
-from praw import Reddit
-from praw.reddit import Submission
+from click import Context
+from praw import Reddit  # type: ignore
+from praw.reddit import Submission, Subreddit  # type: ignore
 from redditmedia import download, get_media, __version__
-from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple
+from typing import Iterable, Tuple
 
 
 @click.group()
@@ -11,7 +12,7 @@ from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple
 @click.option('--path', '-p', default='./reddit-media-downloads', help='path to folder for downloaded media')
 @click.option('--credentials', '-c', type=(str, str), help='explicitly pass Reddit API credentials')
 @click.pass_context
-def main(ctx, output, separate, path, credentials):
+def main(ctx: Context, output: bool, separate: bool, path: str, credentials: Tuple[str, str]):
     """
     Downloads specified reddit submissions media into local folder `reddit-media-downloads`
     (or specified using --path option). For accessing Reddit API credentials should be provided,
@@ -35,12 +36,12 @@ def main(ctx, output, separate, path, credentials):
 
 
 @main.command()
-@click.argument('submissions', type=str, nargs=-1)
+@click.argument('submission-ids', type=str, nargs=-1)
 @click.pass_obj
-def get(obj, submissions):
+def get(obj: dict, submission_ids: Tuple[str]):
     """ Download media from specified submissions """
 
-    submissions = (obj['reddit'].submission(x) for x in submissions)
+    submissions = (obj['reddit'].submission(x) for x in submission_ids)
     process(submissions, obj['output'], obj['path'], obj['separate'])
 
 
@@ -48,7 +49,7 @@ def get(obj, submissions):
 @click.option('--limit', '-n', default=1, help='maximum number of submissions to download')
 @click.argument('subreddit', type=str)
 @click.pass_obj
-def hot(obj, subreddit, limit):
+def hot(obj: dict, subreddit: Submission, limit: int):
     """ Download media of hot submissions in specified subreddit """
 
     if subreddit.startswith('r/'):
@@ -62,7 +63,7 @@ def hot(obj, subreddit, limit):
 @click.option('--limit', '-n', default=1, help='maximum number of submissions to download')
 @click.argument('subreddit', type=str)
 @click.pass_obj
-def new(obj, subreddit, limit):
+def new(obj: dict, subreddit: Submission, limit: int):
     """ Download media of new submissions in specified subreddit """
 
     if subreddit.startswith('r/'):
@@ -74,12 +75,13 @@ def new(obj, subreddit, limit):
 
 time_filters = ['all', 'day', 'hour', 'month', 'week', 'year']
 
+
 @main.command()
 @click.option('--limit', '-n', default=1, help='maximum number of submissions to download')
 @click.option('--time-filter', '-t', default=time_filters[0], type=click.Choice(time_filters, case_sensitive=False))
 @click.argument('subreddit', type=str)
 @click.pass_obj
-def top(obj, subreddit, limit, time_filter):
+def top(obj: dict, subreddit: Subreddit, limit: int, time_filter: str):
     """ Download media of top submissions in specified subreddit """
 
     if subreddit.startswith('r/'):
@@ -89,7 +91,7 @@ def top(obj, subreddit, limit, time_filter):
     process(submissions, obj['output'], obj['path'], obj['separate'])
 
 
-def process(submissions: Iterable[Submission], output, path: str, separate: bool):
+def process(submissions: Iterable[Submission], output: bool, path: str, separate: bool):
     """ Downloads or prints media files of given submissions """
 
     if not output:
